0eca87875e593d95650516887d7cbecd06e9f1c2
kimura <mkimura7878@gmail.com>
2020-09-25 10:14:46 +0000

    Game検索機能を実装した

--- myapp/app/Http/Controllers/GamesController.php
+++ myapp/app/Http/Controllers/GamesController.php
@@ -4,21 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+// use Illuminate\Support\Facades\Storage;
+use App\Models\Game;
+// use App\Recruitment;
+// use Abraham\TwitterOAuth\TwitterOAuth;
 
 class GamesController extends Controller
 {   
     public function index(Request $request)
     {
-        // $keyword = $request->input('keyword');
+        $keyword = $request->input('keyword');
 
-        // // $games = Game::all();
-        // // return view('game/index', compact('games'));
-        // $query = Game::query();
-        // if(!empty($keyword))
-        // {
-        // $query->Where('name','like','%'.$keyword.'%');
-        // }
-        // $games = $query->orderBy('id','desc')->paginate(10);
-        return view('games.index');
+        $query = Game::query();
+        if(!empty($keyword))
+        {
+        $query->Where('name','like','%'.$keyword.'%');
+        }
+        $games = $query->orderBy('id','desc')->paginate(10);
+        return view('games.index', compact('games', 'keyword'));
     }
 }
--- myapp/resources/views/games/index.blade.php
+++ myapp/resources/views/games/index.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
+@section('title', 'ゲーム一覧')
+
 @section('content')
-  <h1>Geme Index</h1>
+  <div class="container">
+    <h2>ゲーム一覧</h2>
+      @if(Session::has('flash'))
+        <div class="alert alert-success" role="alert">
+          {{ session('flash') }}
+        </div>
+      @endif
+
+    <form method="get">
+      @csrf
+      <div class="form-group">
+        <input type="text" name="keyword" value="{{$keyword}}" placeholder="ゲーム検索">
+        <input type="submit" value="検索" class="btn btn-dark btn-sm">
+      </div>
+    </form>
+
+    <a href="/games/create">ゲームタイトルを追加する</a>
+    <br><br>
+      
+    @foreach ($games as $game)
+      <h4><a href="/game/{{$game->id}}">{{$game->name}}</a></h4>
+      <div class="post-image">
+        <img src="{{ $game->image }}" width="30%"/>
+      </div>
+      {{-- <img src="{{ asset('storage/'.$game->image) }}"> --}}
+      {{-- <a href="/game/{{$game->id}}/edit">編集する</a> --}}
+        {{-- <a href="/games/{{$game->id}}">削除する</a> --}}
+      <hr>
+    @endforeach
+  </div>
 @endsection
