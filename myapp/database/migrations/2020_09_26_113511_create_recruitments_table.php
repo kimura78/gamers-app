2f72e4241cd31cf1febea9d838f2c002e0e09e11
kimura <mkimura7878@gmail.com>
2020-09-27 01:53:54 +0000

    Recruitmentモデルを作成した

---
+++ myapp/database/migrations/2020_09_26_113511_create_recruitments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecruitmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recruitments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('start_time', 20);
+            $table->integer('user_id')->unsigned();
+            $table->integer('game_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recruitments');
+    }
+}
